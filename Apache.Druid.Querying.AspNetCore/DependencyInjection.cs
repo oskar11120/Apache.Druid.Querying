157c3cf8ed74a21aaaa6facdf8028b112cf7c3e6
Oskar Furlepa <oskar.furlepa@hitachienergy.com>
2023-12-23 17:52:30 +0100

    Cleanup

--- Apache.Druid.Querying.AspNetCore/DependencyInjection.cs
+++ Apache.Druid.Querying.AspNetCore/DependencyInjection.cs
@@ -1,10 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
-using System.Buffers;
-using System.Diagnostics.CodeAnalysis;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 using Apache.Druid.Querying.DependencyInjection;
-using Apache.Druid.Querying.Elements;
 
 namespace Apache.Druid.Querying.AspNetCore
 {
@@ -72,90 +68,8 @@ namespace Apache.Druid.Querying.AspNetCore
             var clientId = Guid.NewGuid().ToString();
             var clientBuilder = services.AddHttpClient(clientId);
             clientBuilder.ConfigureHttpClient(client => client.BaseAddress = druidApiUri);
-            var serlializerOptions = new JsonSerializerOptions(JsonSerializerDefaults.Web)
-            {
-                DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
-                Converters =
-                {
-                    new JsonStringEnumConverter(),
-                    new PolymorphicSerializer<Filter>(),
-                    new PolymorphicSerializer<Aggregator>(),
-                    new PolymorphicSerializer<PostAggregator>(),
-                    new PolymorphicSerializer<VirtualColumn>(),
-                //    new UnixMilisecondsConverter<DateTimeOffset>(
-                //        DateTimeOffset.FromUnixTimeMilliseconds,
-                //        Utf8Formatter.TryFormat),
-                //    new UnixMilisecondsConverter<DateTime>(
-                //        static miliseconds => DateTimeOffset.FromUnixTimeMilliseconds(miliseconds).UtcDateTime,
-                //        Utf8Formatter.TryFormat)
-                }
-            };
+            var serlializerOptions = DefaultSerializerOptions.Create();
             return new(clientBuilder, serlializerOptions, clientId);
-        }
-
-        private sealed class PolymorphicSerializer<T> : JsonConverter<T> where T : class
-        {
-            private JsonSerializerOptions? withoutPolymorphicSerializer;
-
-            public override T Read(
-                ref Utf8JsonReader reader,
-                Type typeToConvert,
-                JsonSerializerOptions options)
-                => throw new NotSupportedException();
-
-            public override void Write(
-                Utf8JsonWriter writer,
-                [DisallowNull] T value,
-                JsonSerializerOptions options)
-            {
-                var type = value.GetType();
-                if (type == typeof(T))
-                {
-                    if (withoutPolymorphicSerializer is null)
-                    {
-
-                        withoutPolymorphicSerializer = new(options);
-                        withoutPolymorphicSerializer.Converters.Remove(this);
-                    }
-
-                    options = withoutPolymorphicSerializer;
-                }
-
-                JsonSerializer.Serialize(writer, value, type, options);
-            }
-        }
-
-        private sealed class UnixMilisecondsConverter<T> : JsonConverter<T>
-        {
-            public delegate bool TryFormatUTf8(T value, Span<byte> destination, out int bytesWritten, StandardFormat format);
-
-            private readonly Func<long, T> convert;
-            private readonly TryFormatUTf8 tryFormat;
-
-            public UnixMilisecondsConverter(Func<long, T> convert, TryFormatUTf8 tryFormat)
-            {
-                this.convert = convert;
-                this.tryFormat = tryFormat;
-            }
-
-            public override T Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-            {
-                var number = reader.TokenType switch
-                {
-                    JsonTokenType.Number => reader.GetInt64(),
-                    JsonTokenType.String => long.Parse(reader.GetString()!),
-                    _ => throw new NotSupportedException()
-                };
-                return convert(number);
-            }
-
-            public override void Write(Utf8JsonWriter writer, T value, JsonSerializerOptions options)
-            {
-                // The "R" standard format will always be 29 bytes.
-                Span<byte> utf8Date = stackalloc byte[29];
-                tryFormat(value, utf8Date, out _, new StandardFormat('R'));
-                writer.WriteStringValue(utf8Date);
-            }
         }
     }
 }
---
+++ Apache.Druid.Querying/DependencyInjection/DefaultSerializerOptions.cs
@@ -0,0 +1,96 @@
+﻿using Apache.Druid.Querying.Elements;
+using System.Diagnostics.CodeAnalysis;
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Buffers;
+
+namespace Apache.Druid.Querying.DependencyInjection
+{
+    public static class DefaultSerializerOptions
+    {
+        public static JsonSerializerOptions Create() => new(JsonSerializerDefaults.Web)
+        {
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+            Converters =
+            {
+                new JsonStringEnumConverter(),
+                new PolymorphicSerializer<Filter>(),
+                new PolymorphicSerializer<Aggregator>(),
+                new PolymorphicSerializer<PostAggregator>(),
+                new PolymorphicSerializer<VirtualColumn>(),
+                //    new UnixMilisecondsConverter<DateTimeOffset>(
+                //        DateTimeOffset.FromUnixTimeMilliseconds,
+                //        Utf8Formatter.TryFormat),
+                //    new UnixMilisecondsConverter<DateTime>(
+                //        static miliseconds => DateTimeOffset.FromUnixTimeMilliseconds(miliseconds).UtcDateTime,
+                //        Utf8Formatter.TryFormat)
+            }
+        };
+
+        private sealed class PolymorphicSerializer<T> : JsonConverter<T> where T : class
+        {
+            private JsonSerializerOptions? withoutPolymorphicSerializer;
+
+            public override T Read(
+                ref Utf8JsonReader reader,
+                Type typeToConvert,
+                JsonSerializerOptions options)
+                => throw new NotSupportedException();
+
+            public override void Write(
+                Utf8JsonWriter writer,
+                [DisallowNull] T value,
+                JsonSerializerOptions options)
+            {
+                var type = value.GetType();
+                if (type == typeof(T))
+                {
+                    if (withoutPolymorphicSerializer is null)
+                    {
+
+                        withoutPolymorphicSerializer = new(options);
+                        withoutPolymorphicSerializer.Converters.Remove(this);
+                    }
+
+                    options = withoutPolymorphicSerializer;
+                }
+
+                JsonSerializer.Serialize(writer, value, type, options);
+            }
+        }
+
+        private sealed class UnixMilisecondsConverter<T> : JsonConverter<T>
+        {
+            public delegate bool TryFormatUTf8(T value, Span<byte> destination, out int bytesWritten, StandardFormat format);
+
+            private readonly Func<long, T> convert;
+            private readonly TryFormatUTf8 tryFormat;
+
+            public UnixMilisecondsConverter(Func<long, T> convert, TryFormatUTf8 tryFormat)
+            {
+                this.convert = convert;
+                this.tryFormat = tryFormat;
+            }
+
+            public override T Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+            {
+                var number = reader.TokenType switch
+                {
+                    JsonTokenType.Number => reader.GetInt64(),
+                    JsonTokenType.String => long.Parse(reader.GetString()!),
+                    _ => throw new NotSupportedException()
+                };
+                return convert(number);
+            }
+
+            public override void Write(Utf8JsonWriter writer, T value, JsonSerializerOptions options)
+            {
+                // The "R" standard format will always be 29 bytes.
+                Span<byte> utf8Date = stackalloc byte[29];
+                tryFormat(value, utf8Date, out _, new StandardFormat('R'));
+                writer.WriteStringValue(utf8Date);
+            }
+        }
+    }
+}
