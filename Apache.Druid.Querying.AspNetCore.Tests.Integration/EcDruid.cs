2894a05538b17a4d5879ca99a5239e2286b45cf1
Oskar Furlepa <oskar.furlepa@hitachienergy.com>
2024-01-11 12:33:28 +0100

    Typo

--- Apache.Druid.Querying.AspNetCore.Tests.Integration/EcDruid.cs
+++ Apache.Druid.Querying.AspNetCore.Tests.Integration/EcDruid.cs
@@ -3,6 +3,6 @@
     internal sealed class EcDruid : DataSourceProvider
     {
         public DataSource<VariableMessage> Variables => Table<VariableMessage>("data-variables");
-        public DataSource<EventMessage> Events => Table<EventMessage>("data-variables");
+        public DataSource<EventMessage> Events => Table<EventMessage>("data-events");
     }
 }
