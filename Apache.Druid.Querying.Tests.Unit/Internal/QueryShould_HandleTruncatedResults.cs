153b477ab338a0bd0c02a7592ca55d9c5d9551e2
oskarfurlepa <oskarfurlepa@interia.pl>
2024-02-17 22:09:58 +0100

    Truncated result handling tests

--- Apache.Druid.Querying.Tests.Unit/Internal/QueryShould_HandleTruncatedResults.cs
+++ Apache.Druid.Querying.Tests.Unit/Internal/QueryShould_HandleTruncatedResults.cs
@@ -1,6 +1,10 @@
 ﻿using FluentAssertions;
-using Scan = Apache.Druid.Querying.Internal.TruncatedQueryResultHandler<int>.Scan<Apache.Druid.Querying.ScanResult<int>>;
-using TimeSeries = Apache.Druid.Querying.Internal.TruncatedQueryResultHandler<Apache.Druid.Querying.None>.TimeSeries<Apache.Druid.Querying.None>;
+using Scan = Apache.Druid.Querying.Internal.TruncatedQueryResultHandler<Apache.Druid.Querying.None>
+    .Scan<Apache.Druid.Querying.ScanResult<Apache.Druid.Querying.None>>;
+using TimeSeries = Apache.Druid.Querying.Internal.TruncatedQueryResultHandler<Apache.Druid.Querying.None>
+    .TimeSeries<Apache.Druid.Querying.None>;
+using TopN_GroupBy = Apache.Druid.Querying.Internal.TruncatedQueryResultHandler<bool>
+    .TopN_GroupBy<bool, bool, Apache.Druid.Querying.Internal.DimensionsProvider<bool>.Identity>;
 
 namespace Apache.Druid.Querying.Tests.Unit.Internal
 {
@@ -15,14 +19,14 @@ namespace Apache.Druid.Querying.Tests.Unit.Internal
         [Test]
         public async Task Scan()
         {
-            var query = new Query<int>.Scan().Limit(1000) as Scan;
+            var query = new Query<None>.Scan().Limit(1000) as Scan;
             var latest = new Scan.LatestResult();
-            var setter = new Mutable<IQueryWithSource<int>>();
+            var setter = new Mutable<IQueryWithSource<None>>();
             async Task Execute(int start, int count, bool truncate = true)
             {
                 var results = Enumerable
                     .Range(start, count)
-                    .Select(i => new ScanResult<int>(null, i));
+                    .Select(_ => new ScanResult<None>(null, default));
                 (await query
                     .OnTruncatedResultsSetQueryForRemaining(MaybeTruncate(results, truncate), latest, setter, default)
                     .ToListAsync())
@@ -55,15 +59,15 @@ namespace Apache.Druid.Querying.Tests.Unit.Internal
             var latest = new TimeSeries.LatestReturned();
             var setter = new Mutable<IQueryWithSource<None>>();
             var deltaT = TimeSpan.FromHours(1);
-            async Task Execute(IEnumerable<int> source, IEnumerable<int>? results = null, bool truncate = true)
+            async Task Execute(IEnumerable<int> source, bool skipFirstResult = false, bool truncate = true)
             {
-                var results_ = (results ?? source)
+                var results = source
                     .Select(i => new WithTimestamp<None>(t0 + i * deltaT, default));
                 (await query
-                    .OnTruncatedResultsSetQueryForRemaining(MaybeTruncate(results_, truncate), latest, setter, default)
+                    .OnTruncatedResultsSetQueryForRemaining(MaybeTruncate(results, truncate), latest, setter, default)
                     .ToListAsync())
                     .Should()
-                    .BeEquivalentTo(results_);
+                    .BeEquivalentTo(skipFirstResult ? results.Skip(1) : results);
             }
 
             await Execute(Enumerable.Range(0, 24));
@@ -72,17 +76,53 @@ namespace Apache.Druid.Querying.Tests.Unit.Internal
             withIntervals?.Intervals.Should().BeEquivalentTo(
                 new Interval[] { new(t0.AddHours(23), t0.AddDays(2)), new(t0.AddDays(3), t0.AddDays(4)) });
 
-            var source = Enumerable.Range(23, 25).Concat(Enumerable.Range(72, 12));
-            await Execute(source, source.Skip(1));
+            await Execute(Enumerable.Range(23, 25).Concat(Enumerable.Range(72, 12)), skipFirstResult: true);
             latest.Timestamp.Should().Be(t0.AddDays(3).AddHours(11));
             withIntervals = setter.Value as IQueryWith.Intervals;
             withIntervals?.Intervals.Should().BeEquivalentTo(
                 new Interval[] { new(t0.AddDays(3).AddHours(11), t0.AddDays(4)) });
 
-            source = Enumerable.Range(72 + 11, 13);
             setter.Value = null;
-            await Execute(source, source.Skip(1), truncate: false);
+            await Execute(Enumerable.Range(72 + 11, 13), skipFirstResult: true, truncate: false);
             latest.Timestamp.Should().Be(t0.AddDays(4).AddHours(-1));
+            setter.Value.Should().BeNull();
+        }
+
+        [Test]
+        public async Task TopN_GroupBy()
+        {
+            var t0 = DateTime.Today;
+            var latest = new TopN_GroupBy.LatestReturned();
+            var setter = new Mutable<IQueryWithSource<bool>>();
+            var deltaT = TimeSpan.FromHours(1);
+            var query = new Query<bool>.TopN<bool>().Interval(new(t0, t0.AddDays(1))) as TopN_GroupBy;
+            async Task Execute(int start, int count, int skipFirstResults = 0, bool truncate = true)
+            {
+                var results = Enumerable
+                    .Range(start, count)
+                    .Select(i => new WithTimestamp<bool>(t0 + i / 2 * deltaT, i % 2 is 0));
+                (await query
+                    .OnTruncatedResultsSetQueryForRemaining(MaybeTruncate(results, truncate), latest, setter, default)
+                    .ToListAsync())
+                    .Should()
+                    .BeEquivalentTo(results.Skip(skipFirstResults));
+            }
+
+            await Execute(0, 14 + 13);
+            latest.Timestamp.Should().Be(t0.AddHours(13));
+            var withIntervals = setter.Value as IQueryWith.Intervals;
+            withIntervals?.Intervals.Should().BeEquivalentTo(
+                new Interval[] { new(t0.AddHours(13), t0.AddDays(1)) });
+
+            await Execute(14 + 13 - 1, 10, skipFirstResults: 1);
+            latest.Timestamp.Should().Be(t0.AddHours(17));
+            withIntervals = setter.Value as IQueryWith.Intervals;
+            withIntervals?.Intervals.Should().BeEquivalentTo(
+                new Interval[] { new(t0.AddHours(17), t0.AddDays(1)) });
+
+            setter.Value = null;
+            await Execute(14 + 13 - 1 + 10 - 2, 14, skipFirstResults: 2, truncate: false);
+            latest.Timestamp.Should().Be(t0.AddHours(23));
             setter.Value.Should().BeNull();
         }
     }
